07da4349ee7100a573b11c1921932e72c687c673
Jesus Vasquez <adonay16.vz@gmail.com>
2025-06-29 19:31:35 -0600

    feat: middleware to verify auth0 token to access endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { checkJwt } from './common/middleware/auth.middleware'; // 👈 importa tu middleware
 
 @Module({
   imports: [PrismaModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(checkJwt).forRoutes('*');
+  }
+}
